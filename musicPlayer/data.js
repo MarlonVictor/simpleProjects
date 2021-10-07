18c706b6448c17487d8000d003dee3a000b06a0e
MarlonVictor <cmarlonvictor11@gmail.com>
2021-10-07 17:26:25 -0300

    Adding the data with the songs

---
+++ musicPlayer/data.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        title: "Sleep Away",
+        artist: "Desconhecido",
+        cover: "https://images.pexels.com/photos/1535907/pexels-photo-1535907.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        file: "Sleep Away.mp3"
+    },
+    {
+        title: "Kalimba",
+        artist: "Desconhecido",
+        cover: "https://images.pexels.com/photos/383568/pexels-photo-383568.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        file: "Kalimba.mp3"
+    },
+    {
+        title: "Maid with the Flaxen Hair",
+        artist: "Desconhecido",
+        cover: "https://images.pexels.com/photos/137141/pexels-photo-137141.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        file: "Maid with the Flaxen Hair.mp3"
+    }
+]
